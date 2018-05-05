7764999b1f962aa617658267707c8a0856dac6cb
jarcher147 <36341363+jarcher147@users.noreply.github.com>
2018-05-05 15:12:34 -0500

    Updates
    Updating the SOR zero file. Added checkpointing, comments, relative difference.

--- SOR_Zero.m
+++ SOR_Zero.m
@@ -1,4 +1,8 @@
 clear; clc;
+%% Checkpoint Check
+if exist( 'checkpointSOR.mat','file' ) % If a checkpoint file exists, load it
+    load('checkpointSOR.mat')
+end
 %% Define basic parameters
 ax=-pi; %Define lower x bound
 ay=-pi; %Define lower y bound
@@ -7,26 +11,26 @@ by=pi;  %Define upper y bound
 Lx=bx-ax;  %Define length of X
 Ly=by-ay;  %Define length of Y
 %% Nodes
-Nx=80;    %Number of nodes added to the x-axis
+Nx=160;    %Number of nodes added to the x-axis
 Hx=Lx/(1+Nx);    %Length of x-axis segment
 Dx=Hx*Hx;   %Determine delta x squared
-Ny=80;    %Number of nodes added to the y-axis
+Ny=160;    %Number of nodes added to the y-axis
 Hy=Ly/(1+Ny);    %Length of y-axis segment
 Dy=Hy*Hy;   %Determine delta y squared
 %% Create U matrix
 U=zeros(Nx+2,Ny+2); %Preallocate the U matrix
-W=zeros(Nx+2,Ny+2);
+W=zeros(Nx+2,Ny+2); %Preallocate a dummy matrix
 for j=1:Nx+2
     x=Hx*(j-1)+ax;  %Compute the x-value for the given i
     U(j,1)=((x-ax)^2)*sin(pi/2*(x-ax)/(Lx));  %Set BC for y=ay
     U(j,Ny+2)=cos(pi*(x-ax))*cosh(bx-x);    %Set BC for y=by
 end
 %% SOR Loop
-Count=0;
-Max=1;
+Count=0;    %Initialize the count
+Max=1;  %Set Max greater than the limit
 w=1.9;  %Set the coefficient for the over-relaxation
-End=Nx+2;
-while Max > 10^-6
+End=Nx+2;   %Precompute 'N'
+while Max > 10^-9
     for k=2:Ny+1    %All y points not on the boundary
         y=Hy*(k-1)+ay;  %Compute the y-value for the given k
         %BC for x=ax
@@ -40,17 +44,20 @@ while Max > 10^-6
             U(j,k)=-(w-1)*W(j,k)+w*(Dx*(U(j,k-1)+U(j,k+1))+Dy*(U(j-1,k)+U(j+1,k))+(Dx*Dy*F))/(2*(Dx+Dy));
         end
     end
-    Count=Count+1;  %ERROR
-    Diff=W-U;
-    MaxA=max(abs(Diff));
-    Max=max(MaxA);
-    W=U;
-end
+    Count=Count+1;  %Increase the count
+    Max=max(max(abs((W-U)./W)));  %Find the overall max
+    W=U;    %n+1 becomes n
+    if mod(Count,1000)==0   %Save checkpoint file every 1000 iterations
+        save('checkpointSOR.mat'); %Save the file
+    end     %Close if loop
+end     %Close while loop
 %% 3D Plot of the Matrix
-X=-pi:Hx:pi;
-Y=-pi:Hy:pi;
-V=transpose(U);
-h=surf(X,Y,V);
-ylabel('y')
-xlabel('x')
-set(h,'linestyle','none');
+X=-pi:Hx:pi;    %Discretize the X axis
+Y=-pi:Hy:pi;    %Discretize the Y axis
+V=transpose(U); %Transpose the matrix so that the x and y axes are correct
+h=surf(X,Y,V);  %Create surface plot
+ylabel('y') %Label the y-axis
+xlabel('x') %Label the x-axis
+set(h,'linestyle','none');  %Remove the gridlines
+colormap('lines');
+delete('checkpointSOR.mat');    %Delete checkpoint file once evertything is complete
