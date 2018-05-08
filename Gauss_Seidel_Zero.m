63debb9b0188b833df311c954fd8f69400ed1044
jarcher147 <36341363+jarcher147@users.noreply.github.com>
2018-05-08 18:05:54 -0500

    BC, contour plot, checkpointing
    Fixed checkpoint file names, improved contour plot, updated BCs.

--- Gauss_Seidel_Zero.m
+++ Gauss_Seidel_Zero.m
@@ -1,7 +1,7 @@
 clear; clc;
 %% Checkpoint Check
-if exist( 'checkpointGS.mat','file' ) % If a checkpoint file exists, load it
-    load('checkpointGS.mat')
+if exist( 'checkpointGS_zero.mat','file' ) % If a checkpoint file exists, load it
+    load('checkpointGS_zero.mat')
 end
 %% Define basic parameters
 ax=-pi; %Define lower x bound
@@ -23,13 +23,13 @@ W=zeros(Nx+2,Ny+2); %Preallocate a dummy matrix
 for j=1:Nx+2
     x=Hx*(j-1)+ax;  %Compute the x-value for the given i
     U(j,1)=((x-ax)^2)*sin(pi/2*(x-ax)/(Lx));  %Set BC for y=ay
-    U(j,Ny+2)=cos(pi*(x-ax))*cosh(bx-x);    %Set BC for y=by
+    U(j,Ny+2)=(cos(pi*(x-ax))-1)*cosh(bx-x);    %Set BC for y=by
 end
 %% Gauss-Seidel Loop
 Count=0;    %Initialize the count
 Max=1;  %Set Max greater than the limit
 End=Nx+2;   %Precompute 'N'
-while Max > 10^-9
+while Max > 10^-6
     for k=2:Ny+1    %All y points not on the boundary
         y=Hy*(k-1)+ay;  %Compute the y-value for the given k
         %BC for x=ax
@@ -44,19 +44,27 @@ while Max > 10^-9
         end
     end
     Count=Count+1;  %Increase the count
-     Max=max(max(abs((W-U)./W)));  %Find the overall max
+    Max=max(max(abs((W-U)./W)));  %Find the overall max
     W=U;    %n+1 becomes n
     if mod(Count,1000)==0   %Save checkpoint file every 1000 iterations
-        save('checkpointGS.mat'); %Save the file
+        save('checkpointGS_zero.mat'); %Save the file
     end     %Close if loop
 end     %Close while loop
 %% 3D Plot of the Matrix
 X=-pi:Hx:pi;    %Discretize the X axis
 Y=-pi:Hy:pi;    %Discretize the Y axis
 V=transpose(U); %Transpose the matrix so that the x and y axes are correct
+figure()    %First figure
 h=surf(X,Y,V);  %Create surface plot
 ylabel('y') %Label the y-axis
 xlabel('x') %Label the x-axis
 set(h,'linestyle','none');  %Remove the gridlines
-%colormap('gray');
-delete('checkpointGS.mat');    %Delete checkpoint file once evertything is complete
+figure()    %Second figure
+zlevels=[-200,-150,-100,-50,-40,-30,-20,-10,0,10,20,30,40];
+contour(X,Y,V,zlevels,'ShowText','on');  %Create contour plot
+ylabel('y') %Label the y-axis
+xlabel('x') %Label the x-axis
+set(h,'linestyle','none');  %Remove the gridlines
+if exist( 'checkpointGS_zero.mat','file' )
+    delete('checkpointGS_zero.mat');    %Delete checkpoint file once evertything is complete
+end
